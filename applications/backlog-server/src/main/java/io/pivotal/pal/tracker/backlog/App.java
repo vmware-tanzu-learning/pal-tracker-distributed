957cc7f93515436c829df6d252cde486cf49171e
Hector Bernabe <hector.bernabehernandez@schwab.com>
2018-06-07 15:07:08 -0600

    Hystrix circuit breaker for projects

--- applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
+++ applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
@@ -3,6 +3,7 @@ package io.pivotal.pal.tracker.backlog;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,6 +13,7 @@ import java.util.TimeZone;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableCircuitBreaker
 @ComponentScan({"io.pivotal.pal.tracker.backlog", "io.pivotal.pal.tracker.restsupport"})
 public class App {
 
--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,18 +1,29 @@
 package io.pivotal.pal.tracker.allocations;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.client.RestOperations;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
     private final String registrationServerEndpoint;
+    private ConcurrentHashMap<Long, ProjectInfo> cache = new ConcurrentHashMap<>();
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations= restOperations;
         this.registrationServerEndpoint = registrationServerEndpoint;
     }
-
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+
+        ProjectInfo projectInfo = restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        cache.put(projectId, projectInfo);
+        return projectInfo;
+    }
+
+    public ProjectInfo getProjectFromCache(long projectId){
+        return cache.get(projectId);
     }
 }
